b0ac6f0a8377e9ec1caf946ef07c53a108fdcea2
valentin muzyczka <valentin.muzyczka@gmail.com>
2022-01-29 00:25:22 +0100

    first commit

---
+++ src/store/index.js
@@ -0,0 +1,102 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+
+    // CREW
+      Commander :{
+          grade: 'Commander',
+          name: 'Douglas Hurley',
+          resume: 'Douglas Gerald Hurley is an American engineer, former Marine Corps pilot and former NASA astronaut. He launched into space for the third time as commander of Crew Dragon Demo-2.',
+          img: {
+            png: '../assets/crew/image-douglas-hurley.png',
+            webp: '../assets/crew/image-douglas-hurley.webp'
+          }
+        },
+      Engineer :{
+        grade: 'Engineer',
+        name: 'Anousheh Ansari',
+        resume: 'Anousheh Ansari is an Iranian American engineer and co-founder of Prodea Systems. Ansari was the fourth self-funded space tourist, the first self-funded woman to fly to the ISS, and the first Iranian in space.',
+        img: {
+          png: '../assets/crew/image-anousheh-ansari.png',
+          webp: '../assets/crew/image-anousheh-ansari.webp'
+        }
+      },
+      Pilot :{
+        grade: 'Pilot',
+        name: 'Victor Glover',
+        resume: 'Pilot on the first operational flight of the SpaceX Crew Dragon to the International Space Station. Glover is a commander in the U.S. Navy where he pilots an F/A-18.He was a crew member of Expedition 64, and served as a station systems flight engineer. ',
+        img: {
+          png: '../assets/crew/image-victor-glover.png',
+          webp: '../assets/crew/image-victor-glover.webp'
+        }
+      },
+      Specialist :{
+          grade: 'Commander',
+          name: 'Mark Shuttleworth',
+          resume: ' Mark Richard Shuttleworth is the founder and CEO of Canonical, the company behind the Linux-based Ubuntu operating system. Shuttleworth became the first South African to travel to space as a space tourist.',
+          img: {
+            png: '../assets/crew/image-mark-shuttleworth.png',
+            webp: '../assets/crew/image-mark-shuttleworth.webp'
+          }
+        },
+
+
+    // Destination
+
+    Europa :{
+        name: 'Europa',
+        resume: 'The smallest of the four Galilean moons orbiting Jupiter, Europa is a winter lover’s dream. With an icy surface, it’s perfect for a bit of ice skating, curling, hockey, or simple relaxation in your snug wintery cabin.',
+        AvgDist: '628 mil. km',
+        estimatedTravelTime: '3 years'
+      },
+    Mars :{
+        name: 'Mars',
+        resume: 'Don’t forget to pack your hiking boots. You’ll need them to tackle Olympus Mons, the tallest planetary mountain in our solar system. It’s two and a half times the size of Everest!',
+        AvgDist: '225 mil. km',
+        estimatedTravelTime: '9 months'
+      },
+    Moon :{
+        name: 'Moon',
+        resume: 'See our planet as you’ve never seen it before. A perfect relaxing trip away to help regain perspective and come back refreshed. While you’re there, take in some history by visiting the Luna 2 and Apollo 11 landing sites.',
+        AvgDist: '384,400 km',
+        estimatedTravelTime: '3 days'
+      },
+    Titan :{
+        name: 'Titan',
+        resume: 'The only moon known to have a dense atmosphere other than Earth, Titan is a home away from home (just a few hundred degrees colder!). As a bonus, you get striking views of the Rings of Saturn.',
+        AvgDist: '1.6 bil. km',
+        estimatedTravelTime: '7 years'
+      },
+
+
+    // Technology
+    Capsule :{
+      terminology: 'Space capsule',
+      resume: 'A space capsule is an often-crewed spacecraft that uses a blunt-body reentry capsule to reenter the Earth\'s atmosphere without wings. Our capsule is where you\'ll spend your time during the flight. It includes a space gym, cinema, and plenty of other activities to keep you entertained.'
+    },
+    Spaceport :{
+        terminology: 'Spaceport',
+        resume: 'A spaceport or cosmodrome is a site for launching (or receiving) spacecraft, by analogy to the seaport for ships or airport for aircraft. Based in the famous Cape Canaveral, our spaceport is ideally situated to take advantage of the Earth’s rotation for launch.'
+      },
+
+    vehicle :{
+        terminology: 'Launch vehicle',
+        resume: ' A launch vehicle or carrier rocket is a rocket-propelled vehicle used to carry a payload from Earth\'s surface to space, usually to Earth orbit or beyond. Our WEB-X carrier rocket is the most powerful in operation. Standing 150 metres tall, it\'s quite an awe-inspiring sight on the launch pad!'
+      },
+  },
+  getters:{
+    getCommander(state){
+      return `${state.commander}`;
+    }
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+  }
+})
